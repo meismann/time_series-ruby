c67be6ff18375a1f98097134fcb65dab6d7a4738
Martin Eismann <martin.eismann@injixo.com>
2014-12-05 20:41:25 +0100

    Add TimeSeries::TimeSeries.map_with(other).weighted_average

--- lib/time_series/time_series.rb
+++ lib/time_series/time_series.rb
@@ -1,14 +1,67 @@
-class TimeSeries::TimeSeries
+module TimeSeries
+  class TimeSeries
 
-  def initialize(data, start_time, interval)
-    @data, @start_time, @interval = data, start_time, interval
+    attr_reader :start_time, :interval, :data
+
+    def initialize(start_time, interval, data)
+      @start_time, @interval, @data = start_time, interval, data
+    end
+
+    def average
+      sum.to_f / @data.size
+    end
+
+    def sum
+      @data.inject(&:+)
+    end
+
+    def map_with(*time_series, &block)
+      combination = ::TimeSeries::TimeSeriesCombination.new(self, *time_series)
+      return combination.map(&block) if block
+
+      combination
+    end
+
   end
 
-  def average
-    sum.to_f / @data.size
-  end
+  class TimeSeriesCombination
 
-  def sum
-    @data.inject(&:+)
+    def initialize(*time_series)
+      @time_series = time_series
+    end
+
+    def map
+      data_combined = zipDataIterator.map do |values_combined|
+        yield *values_combined
+      end
+      newTimeSeries(data_combined)
+    end
+
+    def weighted_average
+      weighted_sum = zipDataIterator.inject(0) do |_weighted_sum, values|
+        _weighted_sum += values[0] * values[1]
+      end
+      weighted_sum.to_f / secondary_ts.first.sum
+    end
+
+  private
+
+    def newTimeSeries(data)
+      TimeSeries.new(primary_ts.start_time, primary_ts.interval, data)
+    end
+
+    def primary_ts
+      @time_series.first
+    end
+
+    def secondary_ts
+      @time_series[1..-1]
+    end
+
+    def zipDataIterator
+      secondaries_data = secondary_ts.map(&:data)
+      primary_ts.data.zip(*secondaries_data)
+    end
+
   end
 end
--- spec/time_series/time_series_spec.rb
+++ spec/time_series/time_series_spec.rb
@@ -2,11 +2,21 @@ require 'spec_helper'
 
 describe TimeSeries::TimeSeries do
 
-  let(:data) { (0..9).to_a }
+  let(:start_time) { Time.now }
+  let(:interval) { 60 }
+  let(:data) { [0, 1, 2, 3] }
 
-  subject { described_class.new data, Time.now, 60 }
+  subject { described_class.new start_time, interval, data }
 
-  its(:average) { is_expected.to eq 4.5 }
+  its(:average) { is_expected.to eq 1.5 }
 
-  its(:sum) { is_expected.to eq 45 }
+  its(:sum) { is_expected.to eq 6 }
+
+  describe '#map_with(other).weighted_average' do
+    let(:other) { described_class.new start_time, interval, [0, 0, 8, 2] }
+
+    it 'returns the weighted average' do
+      expect(subject.map_with(other).weighted_average).to eq 2.2
+    end
+  end
 end
